10afa462f6c498fd058a082c998bd5076531f7d5
swhan9404 <swhan9404@naver.com>
2021-07-04 18:25:30 +0900

    drf, swagger 추가

--- linewalks/settings.py
+++ linewalks/settings.py
@@ -32,7 +32,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'drf_yasg',
     'patient',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
